735dd56ee282754681d754e38351d480c8f489a4
shashisagar <shashisagar125@gmail.com>
2019-11-24 08:56:53 +0530

    Get basic form value on click email,password

--- src/App.js
+++ src/App.js
@@ -16,33 +16,36 @@ class App extends React.Component {
             email: "",
             password: "",
         }
-        this.onChangeEmail = this.onChangeEmail.bind(this);
+       // this.onChangeEmail = this.onChangeEmail.bind(this);
        // this.onChangePassword=this.onChangePassword.bind(this);
-        this.handleClick = this.handleClick.bind(this);
+        //this.handleClick = this.handleClick.bind(this);
     }
 
     onChangeEmail=(e)=>{
-        alert(e.target.value);
-        this.setState({value: e.target.value});
+       // alert(e.target.value);
+        console.log("here");
+        this.setState({email: e.target.value});
 
     }
 
-    // onChangePassword=(e)=>{
-    //    this.setState({value:e.target.value.password});
-    // }
+    onChangePassword=(e)=>{
+        console.log("there");
+        this.setState({password:e.target.value});
+    }
 
 
     handleClick= (e) => {
        e.preventDefault();
-        alert(this.state.email);
-        alert(this.state.password);
+       alert("here");
+       alert(this.state.email);
+      
     }
 
     render() {
         return (<Form>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" value={this.state.email} onChange={this.onChangeEmail} placeholder="Enter email"/>
+                <Form.Control type="email" onChange={this.onChangeEmail} placeholder="Enter email"/>
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                 </Form.Text>
@@ -50,7 +53,7 @@ class App extends React.Component {
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" value={this.state.password} onChange={this.onChangePassword} placeholder="Password"/>
+                <Form.Control type="password" onChange={this.onChangePassword} placeholder="Password"/>
             </Form.Group>
             <Form.Group controlId="formBasicCheckbox">
                 <Form.Check type="checkbox" label="Check me out"/>
